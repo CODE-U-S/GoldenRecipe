7667ce32418fb947c360343b122f629de4bc2259
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-10-30 19:48:12 +0900

    store create

---
+++ Assets/GameScenes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameScenes : MonoBehaviour
+{
+    public void GameSceneCtr()
+    {
+        SceneManager.LoadScene("Start");
+        Debug.Log("gamescenego+m");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
