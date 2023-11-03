1058875c9a159d1aa84be414f3a8d4907d797afa
Jinseon <128007743+dlwlstjs@users.noreply.github.com>
2023-11-04 00:36:09 +0900

    story 1~7 create

---
+++ Assets/Scripts/story_scripts/Story05.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Story05 : MonoBehaviour
+{
+    void Start()
+    {
+        // 1초 후에 ChangeToMainScene 메서드를 호출
+        Invoke("ChangeToMainScene", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Main 씬으로 전환하는 메서드
+    void ChangeToMainScene()
+    {
+        SceneManager.LoadScene("Story6"); // "Main"은 전환할 씬의 이름입니다.
+    }
+}
