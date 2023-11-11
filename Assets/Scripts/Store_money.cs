22b42e58aaf08e18a91199ee3972f7a6723292c7
true <128007743+dlwlstjs@users.noreply.github.com>
2023-11-11 14:06:27 +0900

    :sparkles:Shop and Home Scene Create

--- Assets/Scripts/Store_money.cs
+++ Assets/Scripts/Store_money.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class Store_money : MonoBehaviour
 {
-    
+
     public TextMeshProUGUI moneybar_text; // UI에서 돈을 표시할 Text 컴포넌트를 연결해줍니다.
     static int money = 100; // 초기 돈 값
 
@@ -13,22 +13,31 @@ public class Store_money : MonoBehaviour
     public void Subtract5()
     {
         Debug.Log("money 5");
-        money -= 5;
-        update(money);
+        if (money >= 5)
+        {
+            money -= 5;
+            update(money);
+        }
     }
 
     public void Subtract10()
     {
         Debug.Log("money 10");
-        money -= 10;
-        update(money);
+        if (money >= 10)
+        {
+            money -= 10;
+            update(money);
+        }
     }
 
     public void Subtract15()
     {
         Debug.Log("money 15");
-        money -= 15;
-        update(money);
+        if (money >= 15)
+        {
+            money -= 15;
+            update(money);
+        }
     }
     public void update(int money)
     {
