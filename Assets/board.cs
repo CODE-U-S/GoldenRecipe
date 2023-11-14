57b98465ef85eafdd3ac7aadcc3deb50b8c04f98
iris-starry <iris_starry_@naver.com>
2023-11-14 22:44:31 +0900

    :sparkles: board 요리 후 클릭시 리셋

--- Assets/board.cs
+++ Assets/board.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class board : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private Sprite defaultSprite; // 기본 이미지를 저장할 변수 추가
     public Sprite board_carrot;
     public Sprite board_garlic;
     public Sprite board_potato;
     public Sprite board_leek;
 
     private Dictionary<string, Sprite> tagToSprite;
-    public AudioClip collisionSound; // 직접 추가한 오디오 클립
+    public AudioClip collisionSound;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        defaultSprite = spriteRenderer.sprite; // 기본 이미지 설정
 
-        // 딕셔너리 초기화
         tagToSprite = new Dictionary<string, Sprite>
         {
             { "carrot", board_carrot },
@@ -30,20 +31,15 @@ public class board : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // 충돌한 오브젝트의 태그를 가져오기
         string collidedTag = collision.tag;
 
-        // 딕셔너리에서 태그에 해당하는 이미지 가져오기
         if (tagToSprite.ContainsKey(collidedTag))
         {
             Sprite newSprite = tagToSprite[collidedTag];
-
-            // 이미지 변경
             spriteRenderer.sprite = newSprite;
 
-            // 직접 추가한 오디오 클립을 재생
             if (collisionSound != null)
-            {   
+            {
                 AudioSource.PlayClipAtPoint(collisionSound, transform.position);
             }
             else
@@ -51,5 +47,11 @@ public class board : MonoBehaviour
                 Debug.LogWarning("board 사운드 없음");
             }
         }
+    }
+
+    // 새로운 함수 추가: 기본 이미지로 되돌리기
+    public void ResetToDefaultSprite()
+    {
+        spriteRenderer.sprite = defaultSprite;
     }
 }
