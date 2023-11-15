5fe570ae8c574a41f93342d4942315397e01a4a7
true <128007743+dlwlstjs@users.noreply.github.com>
2023-11-15 10:19:26 +0900

    setting button 새 기능 추가

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -5,8 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class Setting : MonoBehaviour
 {
-    public void LoadClose()
+    public void DiscordURL()     //링크
     {
-        SceneManager.LoadScene("", LoadSceneMode.Additive);
+        Application.OpenURL("https://discord.gg/pXzaGUrhMR");
+        Debug.Log("dicord");
+    }
+
+    public void GithubURL()    //링크
+    {
+        Application.OpenURL("https://github.com/CODE-U-S/GoldenRecipe");
+        Debug.Log("github");
+    }
+    public void LoadSave()
+    {
+        SceneManager.LoadScene("save", LoadSceneMode.Additive);
+        Debug.Log("save");
+    }
+    public void LoadReset()
+    {
+        SceneManager.LoadScene("reset", LoadSceneMode.Additive);
+        Debug.Log("reset");
+    }
+    public void LoadTiket()
+    {
+        SceneManager.LoadScene("tiket", LoadSceneMode.Additive);
+        Debug.Log("tiket");
     }
 }
