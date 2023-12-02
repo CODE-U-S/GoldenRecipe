497e8b36030abca0734b01d64f82c896fed7aa2c
iris-starry <iris_starry_@naver.com>
2023-12-02 11:10:40 +0900

    :sparkles: 엔딩2 완성

--- Assets/Scripts/List.cs
+++ Assets/Scripts/List.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class List : MonoBehaviour
@@ -8,13 +7,12 @@ public class List : MonoBehaviour
     
     public void MoveLevel1()
     {
-        SceneManager.LoadScene("Level 1");
-        Debug.Log("Level 1으로 전환");
+        SceneManager.LoadScene("level1");
     }
 
     public void MoveLevel2()
     {
-        SceneManager.LoadScene("Level 2");
+        SceneManager.LoadScene("level2");
     }
 
     public void LoadClose()
